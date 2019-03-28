6bdaf5e7dc4a5be0a55361ddb31c3fbf635d7948
idkhbtfm <26010778+InTheFlipside@users.noreply.github.com>
2019-03-28 15:25:23 +0100

    Servomoteur avec les angles bons

---
+++ Splitted Program/servomoteur canon.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+
+  
+myservo.write(50);
+
+delay(5000);
+
+  
+myservo.write(130);
+delay(2500);
+}
