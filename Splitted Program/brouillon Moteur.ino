33030cf047e2a3befc4aae93db8879cf62713b77
idkhbtfm <26010778+InTheFlipside@users.noreply.github.com>
2019-04-04 16:35:27 +0200

    Update brouillon Moteur.ino

--- Splitted Program/brouillon Moteur.ino
+++ Splitted Program/brouillon Moteur.ino
@@ -1,20 +1,47 @@
+
+#include <Servo.h>
+Servo myservo;  
+Servo myservo2;
+int pos;
+int pastservo=0;
+
+int cote;
+int lastetat; 
+int cmpfourche=0;
+
+
+int ledPin = 13;                // choose the pin for the LED
+int inputPin = 3;   
+int emp = 0;
+// Connect sensor to input pin 3 
 int motorPin1 = 40;                                       //Motorpin 1 sera branché sur la broche 40 de la mega
 int motorPin2 = 41;                                       //Motorpin 2 sera branché sur la broche 41 de la mega
 int motorPin3 = 42;                                       //Motorpin 3 sera branché sur la broche 42 de la mega
 int motorPin4 = 43;                                      //Motorpin 4 sera branché sur la broche 43 de la mega
 int ENA=11;   
 int ENB=12;
-int Av = 1;
-int Ar = 2;
-int St = 3;
-int Tu = 4;
+
 int state = 10;
-const int fourchePin = 13;
-const int ledPin =  9;
+const int fourchePin = 8;
 int EtatFourche = 0 ;
 int CMPFOURCHE1 = 0; 
 
+
+
+//Cale (FOURCHE OPTIQUE)
+int cale = 36;
+
 void setup() {
+pastservo=0;
+emp = 0;
+
+pinMode(A5, INPUT_PULLUP);
+
+
+
+  myservo2.attach(10); 
+
+  myservo.attach(9);
 
 pinMode(ENA,OUTPUT);
 pinMode(ENB,OUTPUT);
@@ -22,80 +49,142 @@ pinMode(motorPin1, OUTPUT);
 pinMode(motorPin2, OUTPUT);
 pinMode(motorPin3, OUTPUT);
 pinMode(motorPin4, OUTPUT);
-Serial.begin(9600);
 digitalWrite(ENA,LOW);
 digitalWrite(ENB,LOW);
-pinMode(ledPin, OUTPUT);     //LED en sortie
-pinMode(fourchePin, INPUT);  //en entrée
-Serial.println("Fourche optique - detection de presence");
+ Serial.begin(230400);
+  Serial.println("Initialisation");
+  pinMode(ledPin, OUTPUT);      // declare LED as output
+  pinMode(inputPin, INPUT);     // declare pushbutton as input
+brasup();
+
+while(digitalRead (cale) == 0) {
+  digitalWrite(13, HIGH);
+  delay(20);
+  digitalWrite(13,LOW); }
+
+
+//    delay(5000); 
+  Serial.println("On abaisse le bras");
+brasdown();
+  
+   delay(2000); 
+int cote = digitalRead(A5); // DEFINIR SENS DU SWITCH
+
+
+
+
 
 }
 
-void loop() {
-
-state = Serial.parseInt();
-EtatFourche = digitalRead(fourchePin);
-Serial.print("Etat");
+void loop(){
 
 
 
-if (EtatFourche < 40) {  
 
-EtatFourche=EtatFourche ++;
-Serial.println(EtatFourche);  
-} 
-
-
-if (EtatFourche = 40) {
-digitalWrite(motorPin1, LOW); 
-digitalWrite(motorPin2, LOW);
-digitalWrite(motorPin3, LOW); 
-digitalWrite(motorPin4, LOW); 
+if (pastservo != 1){
+ brasdown();
 }
 
 
-if (state == Av){Serial.println("Avant");
 
-digitalWrite(motorPin1, LOW); 
+  
+  int val = digitalRead(inputPin);  // read input value
+  if (val == HIGH) {       
+   pastservo=1;
+    myservo.write(90);
+  digitalWrite(motorPin1, HIGH); 
 digitalWrite(motorPin2, HIGH);
 digitalWrite(motorPin3, HIGH); 
-digitalWrite(motorPin4, LOW);
-analogWrite(ENA,60);
-analogWrite(ENB,60);
-}                                     
-
-if (state == Ar){Serial.println("Arriere");
-
-digitalWrite(motorPin1, HIGH); 
-digitalWrite(motorPin2, LOW);
-digitalWrite(motorPin3, LOW); 
 digitalWrite(motorPin4, HIGH);
+digitalWrite(ENA,LOW);
+digitalWrite(ENB,LOW);
 
-analogWrite(ENA,60);
-analogWrite(ENB,60);
 
-}
+ if (cote != 1) { turnright(); Serial.println("TOURNE A GAUCHE"); }
+ if (cote == 1) { turnleft(); Serial.println("TOURNE A DROITE"); }
 
-if (state == St){Serial.println("Stop"); 
-
-digitalWrite(motorPin1, LOW); 
-digitalWrite(motorPin2, LOW);
-digitalWrite(motorPin3, LOW); 
-digitalWrite(motorPin4, LOW);
-
-}
-
-if (state == Tu) {Serial.println("Turn");
-
-digitalWrite(motorPin1, HIGH); 
+    
+    delay(795);
+ digitalWrite(motorPin1, HIGH); 
 digitalWrite(motorPin2, LOW);
 digitalWrite(motorPin3, HIGH); 
 digitalWrite(motorPin4, LOW);
+analogWrite(ENA,100);
+analogWrite(ENB,100); 
+emp = 1;
+Serial.println("Emp changé");
+  /// ON APPUIE 
+  } else {
+
+//  ON APPUIE PAS
+
+  digitalWrite(motorPin1, LOW); 
+digitalWrite(motorPin2, HIGH);
+digitalWrite(motorPin3, LOW); 
+digitalWrite(motorPin4, HIGH);
+analogWrite(ENA,100);
+analogWrite(ENB,100); 
+ 
 
 
-analogWrite(ENA,60);
-analogWrite(ENB,60);
+if (emp != 0) {
+  
+  EtatFourche = digitalRead(fourchePin);
+  if(EtatFourche == 1 && EtatFourche != lastetat) { cmpfourche = cmpfourche+1;  Serial.println("+1");  }
+
+   
+Serial.print("          CMP: ");
+Serial.println(cmpfourche);
+while( cmpfourche > 40000){
+ Serial.println("Mission accomplie");
+
+
+
+digitalWrite(motorPin1, HIGH); 
+digitalWrite(motorPin2, HIGH);
+digitalWrite(motorPin3, HIGH); 
+digitalWrite(motorPin4, HIGH);
+digitalWrite(ENA,LOW);
+digitalWrite(ENB,LOW);
+
+exp2();
 
 }
+   
+              }
+
+
+lastetat = EtatFourche;    
+  } // FIN DU BOUTON
+  
+} //// FIN DU VOIDLOOP
+
+
+void turnleft(){ digitalWrite(motorPin1, HIGH); digitalWrite(motorPin2, LOW); digitalWrite(motorPin3, LOW); digitalWrite(motorPin4, HIGH);
+analogWrite(ENA,100);
+analogWrite(ENB,100);
+delay(50);
 }
 
+
+void turnright() { digitalWrite(motorPin1, LOW); digitalWrite(motorPin2, HIGH); digitalWrite(motorPin3, HIGH); digitalWrite(motorPin4, LOW);
+analogWrite(ENA,100);
+analogWrite(ENB,100);
+delay(50);
+}
+
+void brasdown() {myservo.write(170);}
+void brasup() {myservo.write(90);}
+
+
+void exp2() {
+   for (pos = 30; pos <= 150; pos += 1) { 
+    // in steps of 1 degree
+    myservo2.write(pos);              
+    delay(10);                       
+  }
+  for (pos = 150; pos >= 30; pos -= 1) { 
+    myservo2.write(pos);          
+delay(10); 
+}
+            }
