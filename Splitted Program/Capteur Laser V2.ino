7c7e1d024e402b9a6f39449b46a45a23437788ca
idkhbtfm <26010778+InTheFlipside@users.noreply.github.com>
2019-05-09 16:33:06 +0200

    Update Capteur Laser V2.ino

--- Splitted Program/Capteur Laser V2.ino
+++ Splitted Program/Capteur Laser V2.ino
@@ -7,17 +7,21 @@ The range readings are in units of mm. */
 #include <Wire.h>
 #include <VL53L0X.h>
 
-
+int cote = 1;
 VL53L0X sensorA;
 VL53L0X sensorB;
+VL53L0X sensorC;
+
 void setup()
-{  pinMode(5, OUTPUT);
+{  
 
 
   Serial.begin(9600);
   Wire.begin();
 
-digitalWrite(5, LOW);
+if (cote == 1) {
+
+
   sensorA.init();
   sensorA.setTimeout(500);
   sensorA.setAddress(29);
@@ -26,27 +30,54 @@ digitalWrite(5, LOW);
   Serial.print("Adresse du capteur 1: ");
   Serial.println(sensorA.getAddress());
   
-digitalWrite(5, HIGH);
+
   sensorB.init();
   sensorB.setTimeout(500);
   sensorB.setAddress(28);
   sensorB.startContinuous();
   delay(200);
   Serial.print("Adresse du capteur 2: ");
-  Serial.println(sensorB.getAddress());
+  Serial.println(sensorB.getAddress()); }
+  
+if (cote == 0) {
+  sensorC.init();
+  sensorC.setTimeout(500);
+  sensorC.setAddress(27);
+  sensorC.startContinuous();
+  delay(200);
+  Serial.print("Adresse du capteur 3: ");
+  Serial.println(sensorC.getAddress()); }
+  
 
 }
 
 void loop()
 {
+
+  if (cote == 1) {
 Serial.print("Capteur du haut:   ");
     Serial.print(sensorA.readRangeContinuousMillimeters());
   if (sensorA.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
   delay(10);
 
 Serial.print("               Capteur du bas:   ");
-pinMode(5, INPUT);
     Serial.println(sensorB.readRangeContinuousMillimeters());
   if (sensorB.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
-delay(10);
+delay(10);  }
+
+if (cote == 0){
+Serial.print("               Capteur du Milieu:   ");
+    Serial.println(sensorC.readRangeContinuousMillimeters());
+  if (sensorC.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+delay(10); 
+  
+}
+
+
+
+
+
+
+
+  
 }
